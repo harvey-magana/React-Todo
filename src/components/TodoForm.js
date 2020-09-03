0ff5a5df29f286bb41a0a515bd5e7d58319c542f
harvey magana <heyharv2012@gmail.com>
2020-09-02 21:06:53 -0700

    Updated the TodoForm component with handleChanges and handleSubmit methods.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -8,10 +8,30 @@ class TodoForm extends React.Component {
         }
     }
 
+    handleChanges = (e) => {
+        this.setState({
+            item: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.todos.push({
+            todo: this.state.item, 
+            completed: false,
+            id: Date.now()
+        })
+    }
+
     render() {
         return (
             <form>
-                <input />
+                <input 
+                    type="text"
+                    todo="item"
+                    value={this.state.item}
+                    onChange={this.handleChanges}
+                />
                 <button>Add</button>
             </form>
         )
